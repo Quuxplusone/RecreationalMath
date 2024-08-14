f9bc60962fa67b782b00621c1d63f11729b79240
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2024-08-14 12:53:08 -0400

    A373811: Initial commit

---
+++ A373811/README.md
@@ -0,0 +1,6 @@
+
+http://list.seqfan.eu/pipermail/seqfan/2024-August/075205.html
+https://oeis.org/A373811
+
+a(0) = 0. For n > 0, a(n) is the smallest number of straight lines needed
+to intersect all points (k, a(k)) for k < n.
---
+++ A373811/compute-and-draw.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+# https://oeis.org/A373811
+# a(0) = 0
+# a(n) = the minimum number of lines needed to pass through all the points (i, a(i)) for 0 <= i < n
+#
+# a(n) = 0, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, ...
+
+# Represent a point as (x,y)
+# Represent a line as (p1, p2)
+
+import matplotlib
+import matplotlib.pyplot as plt
+
+def point_is_on_line(p0, l):
+  p1 = l[0]
+  p2 = l[1]
+  if p0 in [p1, p2]:
+    return True
+  if p0[0] == p1[0]:
+    return p0[0] == p2[0]
+  elif p0[0] == p2[0]:
+    return p0[0] == p1[0]
+  m01 = (p0[1] - p1[1]) * (p0[0] - p2[0])
+  m02 = (p0[1] - p2[1]) * (p0[0] - p1[0])
+  return m01 == m02
+
+def line_between_points(p, q):
+  return (p, q)
+
+def lines_through_points(ps):
+  # We must have a line through ps[0]. Where else?
+  if not ps:
+    return []
+  elif len(ps) == 1:
+    return [(ps[0], ps[0])]
+  best = None
+  for i in range(1, len(ps)):
+    line = line_between_points(ps[0], ps[i])
+    remaining_ps = [p for p in ps if not point_is_on_line(p, line)]
+    lines = lines_through_points(remaining_ps) + [line]
+    if best is None or len(best) > len(lines):
+      best = lines
+  return best
+
+def draw_plot(points, lines):
+  plt.clf()
+  plt.gca().xaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+  plt.gca().yaxis.set_major_locator(matplotlib.ticker.MaxNLocator(integer=True))
+  plt.gca().set_prop_cycle(None)
+  for line in lines:
+    p = [p for p in points if point_is_on_line(p, line)][0]
+    q = [q for q in points if point_is_on_line(q, line)][-1]
+    plt.plot([p[0], q[0]], [p[1], q[1]])
+  plt.plot([p[0] for p in points], [p[1] for p in points], 'ok')
+  plt.savefig('A373811-%d.png' % len(points))
+
+
+if __name__ == '__main__':
+  points = [(0,0), (1,1)]
+  for i in range(100):
+    lines = lines_through_points(points)
+    draw_plot(points, lines)
+    newpoint = (len(points), len(lines))
+    points += [newpoint]
+    print(points)
